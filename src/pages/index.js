96886e36f30ae3d524195fd4db141a84aa4b0e7a
Ruairi <ruairilane@gmail.com>
2021-04-14 13:00:18 +0100

    Gatsby tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import Header from './components/Header'
+import { Link } from 'gatsby'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return(
+        <div style={{ color: `purple`}}>
+          <Link to="/contact/">Contact</Link>
+          <span> </span>
+          <Link to="/about/">About</Link>
+          <Header headerText="Hello Dear Gatsby!"/>
+          <p>what a world.</p>
+          <img src="https://source.unsplash.com/random/400x200" alt="" />
+          </div>
+  )
 }
